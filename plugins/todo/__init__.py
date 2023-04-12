775a8f8c31da3ee466a3e9472ab6df6ac00cf00f
Ibrahim Al Kurdi <ibrahimalkurdi@gmail.com>
2023-04-12 10:38:19 +0200

    Plugin adjustment to show comment in the tool bar

--- plugins/todo/__init__.py
+++ plugins/todo/__init__.py
@@ -1,5 +1,5 @@
 """
-This extension provides a quick run for my todo script and passing the TODO new item from Albert pop up window.
+This extension provides a quick run for my todo script and passing the TODO new item from Albert application launcher.
 """
 
 from albert import *
@@ -7,13 +7,14 @@ import subprocess
 import sys
 
 md_iid = "0.5"
-md_version = "1.2"
+md_version = "0.2"
 #md_id = "overwrite"
-md_name = "TDOD script"
-md_description = "TDOD addition tasks to my automated TODO file"
+md_name = "TODO List"
+md_description = "Automated task addition to my TODO list file"
 md_license = "BSD-2"
 md_url = "https://github.com/ibrahimalkurdi/albert-todo-plugin"
 md_maintainers = "@ibrahimalkurdi"
+
 
 class Plugin(QueryHandler):
     def id(self):
@@ -31,12 +32,17 @@ class Plugin(QueryHandler):
     def finalize(self):
         info('finalize')
 
+    def synopsis(self):
+        return '[[Add your Todo list item]]'
+
     def handleQuery(self, query):
+        if not query.isValid:
+            return
         if query.string.strip():
             args = query.string.strip()
         query.add(Item(
             id="Id",
-            text="Text",
-            subtext="Subtext",
-            actions=[Action('Run script', 'Run Script', lambda: subprocess.Popen(['bash', '~/automated_todo_list.sh', args  ]) )]
+            text="Task Management",
+            subtext="Each entry will add single task to the automated TODO list",
+            actions=[Action('Run script', 'Run Script', lambda: subprocess.Popen(['bash', '/home/ibrahim/scripts/automated_todo_list.sh', args  ]) )]
         ))
