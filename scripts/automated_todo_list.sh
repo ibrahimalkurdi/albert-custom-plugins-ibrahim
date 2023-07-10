b4ba88c5d714eff5a4420ce58fd708be50819ecb
Ibrahim Al Kurdi <37270645+ibrahimalkurdi@users.noreply.github.com>
2023-07-10 22:20:00 +0200

    Update automated_todo_list.sh

--- scripts/automated_todo_list.sh
+++ scripts/automated_todo_list.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+my_todo_list_file="/home/<user>/Documents/<usere>-workspace/Visual-Studio-code/my-schedule-tasks.todo"
+current_date="$(date +%d/%m/%Y)"
+ensure_date_existence=$(grep ${current_date} ${my_todo_list_file} | wc -l)
+
 if [[ ! -z $@ ]]; then
-	echo -e "    ☐ $@" >> ~/automated-tasks.todo
+  if [[ ${ensure_date_existence} -eq 0 ]]; then
+    echo -e "\n${current_date}:" >> ${my_todo_list_file}
+    echo -e "    ☐ $@" >> ${my_todo_list_file}
+  else
+    echo -e "    ☐ $@" >> ${my_todo_list_file}
+  fi
 fi
